e87fcce2d72844c417e676a6df356ad2e42991f2
Sean Sundberg <seansund@us.ibm.com>
2020-06-30 21:40:50 -0500

    Switches back to hosted sysdig agent script (#1)

--- scripts/bind-sysdig.sh
+++ scripts/bind-sysdig.sh
@@ -8,13 +8,10 @@ NAMESPACE="$3"
 CLUSTER_TYPE="$4"
 
 if [[ "${CLUSTER_TYPE}" != "kubernetes" ]]; then
-   OPENSHIFT="-op"
+   OPENSHIFT="--openshift"
 fi
 
 echo "*** Binding sysdig to cluster namespace ${NAMESPACE} using endpoint: ${ENDPOINT}"
 
-#curl -sL https://ibm.biz/install-sysdig-k8s-agent | \
-#  bash -s -- -a "${ACCESS_KEY}" -c "${ENDPOINT}" -ns "${NAMESPACE}" ${OPENSHIFT} -ac 'sysdig_capture_enabled: false'
-
-cat ${SCRIPT_DIR}/install-agent-k8s.sh | \
+curl -sL https://ibm.biz/install-sysdig-k8s-agent | \
   bash -s -- -a "${ACCESS_KEY}" -c "${ENDPOINT}" -ns "${NAMESPACE}" ${OPENSHIFT} -ac 'sysdig_capture_enabled: false'
